efc3e66083159386a741760ac25c5e7eb4bb8b5b
Jobin Jose <jobin.jose@logidots.com>
2022-03-10 11:57:40 +0530

    message filed added

--- src/Engagespot.php
+++ src/Engagespot.php
@@ -5,28 +5,29 @@ use Illuminate\Support\Facades\Http;
 
 class Engagespot{
 
-    public function send(){
-        $notifcationSpec = [
-            'notification' => [
-                "title" => $this->title,
-                "url" => $this->url,
-                "icon" => $this->icon,
-            ],
-            "recipients" => [
-                $this->recipients
-            ],
-            "override" => [
-                "channels" => [
-                    $this->channels
+    public function send()
+    {
+        $notifcationSpec = ['notification' => [
+                    "title" => $this->title,
+                    "message" => $this->message,
+                    "url" => $this->url,
+                    "icon" => $this->icon,
+                ],
+                "recipients" => [
+                    $this->recipients
+                ],
+                "override" => [
+                    "channels" => [
+                        $this->channels
+                    ]
                 ]
-            ]
-        ];
+            ];
         try{
             $response = Http::withHeaders([
                 'X-ENGAGESPOT-API-KEY' => config('engagespot.X-ENGAGESPOT-API-KEY'),
                 'X-ENGAGESPOT-API-SECRET' => config('engagespot.X-ENGAGESPOT-API-SECRET'),
             ])->post('https://api.engagespot.co/v3/notifications', $notifcationSpec);
-
+            
             return response()->json([
                 'code' => $response->getStatusCode(),
                 'message' => $response->getReasonPhrase(),
