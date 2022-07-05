35d1d2297c65c98f65cec33351e3a1aaf6c7d979
Jobin Jose <jobin.jose@logidots.com>
2022-07-05 18:31:26 +0530

    sendgrid email object added

--- src/Engagespot.php
+++ src/Engagespot.php
@@ -1,25 +1,30 @@
 <?php
+
 namespace  Jasjbn\Engagespot;
+
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 
-class Engagespot{
+class Engagespot
+{
 
     public function send()
     {
-        $notifcationSpec = ['notification' => [
-                    "title" => $this->title,
-                    "message" => $this->message ?? null,
-                    "url" => $this->url ?? null,
-                    "icon" => $this->icon ?? null,
-                ],
-                "recipients" => $this->recipients,
-                "category" => $this->category ?? null,
-                "override" => [
-                    "channels" => $this->channels ?? null,
-                ]
-            ];
-        try{
+        $notifcationSpec = [
+            'notification' => [
+                "title" => $this->title,
+                "message" => $this->message ?? null,
+                "url" => $this->url ?? null,
+                "icon" => $this->icon ?? null
+            ],
+            "recipients" => array($this->recipients),
+            "category" => $this->category ?? null,
+            "override" => [
+                "channels" => $this->channels ?? null,
+                "sendgrid_email" => $this->SendGridObject ?? null,
+            ],
+        ];
+        try {
             $response = Http::withHeaders([
                 'X-ENGAGESPOT-API-KEY' => config('engagespot.X-ENGAGESPOT-API-KEY'),
                 'X-ENGAGESPOT-API-SECRET' => config('engagespot.X-ENGAGESPOT-API-SECRET'),
@@ -28,12 +33,9 @@ class Engagespot{
             return response()->json([
                 'code' => $response->getStatusCode(),
                 'message' => $response->body(),
-            ]); 
-
-        }catch(\Exception $e){
+            ]);
+        } catch (\Exception $e) {
             return $e->getMessage();
         }
-
     }
-
 }
